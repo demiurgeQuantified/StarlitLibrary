01538caa37f17ebd52d8124d5be01de3be28cab2
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2025-01-04 08:13:26 +0000

    IsoObjectUtils: prevent chunk lower size limit from becoming a negative odd number, as this causes lighting bugs later

--- Contents/mods/StarlitLibrary/42/media/lua/shared/Starlit/IsoObjectUtils.lua
+++ Contents/mods/StarlitLibrary/42/media/lua/shared/Starlit/IsoObjectUtils.lua
@@ -1,9 +1,9 @@
 local IsoObjectUtils = {}
 
 ---@type IsoCell
-local cell
+local CELL
 Events.OnPostMapLoad.Add(function (_cell, x, y)
-    cell = _cell
+    CELL = _cell
 end)
 
 local MIN_HEIGHT = -32
@@ -14,7 +14,16 @@ local MIN_HEIGHT = -32
 ---@param z integer Z world coordinate
 ---@return IsoGridSquare square The square that was added
 IsoObjectUtils.addSquare = function(x, y, z)
-    return cell:createNewGridSquare(x, y, z, true)
+    if z < 0 and z % 2 == 1 then -- negative odd number
+        -- prevent min level from becoming odd
+        -- if it does, when the next lowest level is created its lighting will be severely bugged
+        local chunk = CELL:getChunkForGridSquare(x, y, z)
+        local minLevel = chunk:getMinLevel()
+        if minLevel < z - 1 then
+            chunk:setMinMaxLevel(z - 1, chunk:getMaxLevel())
+        end
+    end
+    return CELL:createNewGridSquare(x, y, z, true)
 end
 
 ---Gets or creates a square at the given coordinates.
