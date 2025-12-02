53f74667afdc9af98699b5185f2ca7c74373ca1b
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2025-12-02 21:37:58 +0000

    start on new Task API

--- Contents/mods/StarlitLibrary/42/media/lua/shared/Starlit/TaskManager.lua
+++ Contents/mods/StarlitLibrary/42/media/lua/shared/Starlit/TaskManager.lua
@@ -1,14 +1,64 @@
----@class starlit.TaskManager.Task
+---@namespace starlit
+
+
+---@class TaskManager.Task
 ---@field func function
 ---@field args table
 
----@class starlit.TaskManager.RepeatTasks
+
+---@class TaskManager.RepeatTasks
 ---@field offset number
 ---@field [integer] starlit.TaskManager.Task
+
+
+---@class taskmanager.TaskChain
+---@field name string
+---@field tasks fun():TaskManager.TaskResult
+
 
 local currentTick = 0
 
 local TaskManager = {}
+
+
+---Result of a task. Determines what the task manager will do with the task after it completes.
+---@enum TaskManager.TaskResult
+TaskManager.TaskResult = {
+    ---Remove the task from the task manager.
+    DONE = "done",
+    ---Perform the task again next tick.
+    CONTINUE = "continue"
+}
+
+
+---@type (fun():TaskManager.TaskResult)[]
+local tasks = {}
+
+
+---@param func fun():TaskManager.TaskResult
+function TaskManager.addTask(func)
+    tasks[#tasks + 1] = func
+end
+
+
+local function updateTasks()
+    for i = #tasks, 1, -1 do
+        local success, result = pcall(tasks[i])
+        if not success then
+            ---@cast result string
+            print("Error occured in task: " .. result)
+            table.remove(tasks, i)
+        elseif result == TaskManager.TaskResult.DONE then
+            table.remove(tasks, i)
+        else
+            assert(
+                result == TaskManager.TaskResult.CONTINUE,
+                "Task did not return a valid result"
+            )
+        end
+    end
+end
+
 
 
 ---@type table<number, starlit.TaskManager.Task[]>
@@ -21,10 +71,11 @@ TaskManager.repeatTasks = {}
 
 ---Creates a task to repeat a function every N ticks.
 ---Note that it is not guaranteed that every invocation is exactly the given number of ticks apart.
----@param func function The function to call.
+---@generic T...
+---@param func fun(...: T...) The function to call.
 ---@param ticks integer How often, in ticks, to call the function.
----@param ... any Any arguments to the function.
-TaskManager.repeatEveryTicks = function(func, ticks, ...)
+---@param ... T... Any arguments to the function.
+function TaskManager.repeatEveryTicks(func, ticks, ...)
     local repeatFunctions = TaskManager.repeatTasks[ticks]
     if not repeatFunctions then
         repeatFunctions = table.newarray()
@@ -36,10 +87,11 @@ end
 
 
 ---Creates a task to call a function after a delay of N ticks.
----@param func function The function to call.
+---@generic T...
+---@param func fun(...: T...) The function to call.
 ---@param ticks integer The amount of ticks to delay the calling by.
----@param ... any Any arguments to the function.
-TaskManager.delayTicks = function(func, ticks, ...)
+---@param ... T... Any arguments to the function.
+function TaskManager.delayTicks(func, ticks, ...)
     local tasks = TaskManager.delayTasks[currentTick + ticks]
     if not tasks then
         tasks = table.newarray()
@@ -50,8 +102,8 @@ TaskManager.delayTicks = function(func, ticks, ...)
 end
 
 
----@type Callback_OnTick
-TaskManager.update = function(ticks)
+---@param ticks number
+function TaskManager.update(ticks)
     currentTick = ticks
 
     -- run repeat tasks
@@ -90,6 +142,8 @@ TaskManager.update = function(ticks)
         end
         TaskManager.delayTasks[currentTick] = nil
     end
+
+    updateTasks()
 end
 
 Events.OnTick.Add(TaskManager.update)
