34c87dfcdf16e634b4f48eb95ddc6b74d660b7a7
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2025-12-02 02:59:22 +0000

    fix nested it blocks

--- tests/zedscript_spec.lua
+++ tests/zedscript_spec.lua
@@ -25,20 +25,20 @@ insulate("ZedScript module", function()
 
     local ZedScript = require("Starlit/ZedScript")
 
-    it("can load zedscript chunks", function()
+    describe("can load zedscript chunks", function()
         local chunk = ZedScript.loadChunk(ZEDSCRIPT)
 
-        it("can read key-value pairs", function()
+        describe("can read key-value pairs", function()
             assert.are.equal(chunk.pairs["VERSION"], "1")
         end)
 
-        it("can read blocks", function()
+        describe("can read blocks", function()
             local block = chunk.blocks[1]
             assert.are.equal(block.type, "option")
             assert.are.equal(block.id, "TestMod.TestOption")
             assert.are.equal(block.pairs["foo"], "bar")
 
-            it("can read anonymous blocks", function()
+            describe("can read anonymous blocks", function()
                 local block = block.blocks[1]
                 assert.are.equal(block.type, "")
                 assert.are.equal(block.id, "")
@@ -46,11 +46,11 @@ insulate("ZedScript module", function()
                 assert.are.equal(block.pairs["c"], "d")
             end)
 
-            it("can read type-only blocks", function()
+            describe("can read type-only blocks", function()
                 local block = block.blocks[2]
                 assert.are.equal(block.type, "type")
                 assert.are.equal(block.id, "")
-                it("can read non-key-value elements", function()
+                describe("can read non-key-value elements", function()
                     assert.are.equal(block.values[1], "1")
                     assert.are.equal(block.values[2], "2")
                     assert.are.equal(block.values[3], "3")
