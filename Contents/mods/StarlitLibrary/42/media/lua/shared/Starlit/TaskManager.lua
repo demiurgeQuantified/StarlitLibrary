8e58bf366b1139f059e03797d76e1eb3c113f0ff
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2025-12-02 23:17:29 +0000

    add new task api

--- Contents/mods/StarlitLibrary/42/media/lua/shared/Starlit/TaskManager.lua
+++ Contents/mods/StarlitLibrary/42/media/lua/shared/Starlit/TaskManager.lua
@@ -1,6 +1,10 @@
 ---@namespace starlit
 
 
+-- TODO: api for 'sub loggers' for categories like this
+local log = require("Starlit/debug/Logger").getLogger("StarlitLibrary] [TaskManager")
+
+
 ---@class TaskManager.Task
 ---@field func function
 ---@field args table
@@ -11,18 +15,37 @@
 ---@field [integer] starlit.TaskManager.Task
 
 
+---@class taskmanager.Task<T...>
+---@field func fun(...:T...):taskmanager.TaskResult
+---@field args [T...]
+---@field name string
+---@field removed boolean Whether the task should be considered removed. Set when it is not safe to remove the task immediately.
+
+
 ---@class taskmanager.TaskChain
 ---@field name string
----@field tasks fun():TaskManager.TaskResult
+---@field tasks taskmanager.Task[]
+---@field taskMap table<string, taskmanager.Task>
 
 
 local currentTick = 0
 
+---@type table<string, taskmanager.TaskChain>
+local chains = {}
+
+
+---@type string?
+local executingChain = nil
+
+
 local TaskManager = {}
 
 
----Result of a task. Determines what the task manager will do with the task after it completes.
----@enum TaskManager.TaskResult
+---.. versionadded:: v1.5.0
+---
+---Result values for a task. Determines what the task manager will do with the task after it completes.
+---A task that does not return one of these values will error!
+---@enum taskmanager.TaskResult
 TaskManager.TaskResult = {
     ---Remove the task from the task manager.
     DONE = "done",
@@ -31,32 +54,152 @@ TaskManager.TaskResult = {
 }
 
 
----@type (fun():TaskManager.TaskResult)[]
-local tasks = {}
+---.. versionadded:: v1.5.0
+---
+---Creates a task chain. It is necessary to call this function to create a task chain before adding tasks to it.
+---There cannot be more than one task chain with the same name. An error will be raised if a duplicate is created.
+---@param name string Name of the task chain. The format 'modname.modulename' is recommended to avoid overlap.
+function TaskManager.addTaskChain(name)
+    assert(
+        chains[name] == nil,
+        "task chain " .. name .. " already exists"
+    )
+
+    chains[name] = {
+        name = name,
+        tasks = table.newarray(),
+        taskMap = {}
+    }
+end
+
+---.. versionadded:: v1.5.0
+---
+---Adds a task.
+---The task's function will be called every tick until it returns :lua:data:`starlit.taskmanager.TaskResult.DONE`.
+---@generic T...
+---@param chain string Name of the task chain to add the task to. If no chain by that name exists an error will be raised.
+---@param func fun(...:T...):taskmanager.TaskResult Function to call when running the task.
+---@param ... T... Arguments to call func with.
+---@return string name Name of the task. This can be used to query the task later.
+function TaskManager.addTask(chain, func, ...)
+    local chainObj = chains[chain]
+    assert(
+        chainObj ~= nil,
+        "task chain " .. chain .. " does not exist"
+    )
+
+    ---@type taskmanager.Task
+    local task = {
+        func = func,
+        args = {...},
+        name = getRandomUUID(),
+        removed = false
+    }
+
+    chainObj.tasks[#chainObj.tasks + 1] = task
+    chainObj.taskMap[task.name] = task
+    return task.name
+end
 
 
----@param func fun():TaskManager.TaskResult
-function TaskManager.addTask(func)
-    tasks[#tasks + 1] = func
+---.. versionadded:: v1.5.0
+---
+---Removes a task by name.
+---@param chain string Name of the task chain to remove the task from. If no chain by that name exists an error will be raised.
+---@param name string Name of the task to remove. If no task by that name exists an error will be raised.
+function TaskManager.removeTask(chain, name)
+    local chainObj = chains[chain]
+    assert(
+        chainObj ~= nil,
+        "task chain " .. chain .. " does not exist"
+    )
+
+    local task = chainObj.taskMap[name]
+    assert(
+        task ~= nil,
+        "task chain has no such task " .. name
+    )
+
+    if chain == executingChain then
+        task.removed = true
+        return
+    end
+
+    for i = 1, #chainObj.tasks do
+        if chainObj.tasks[i] == task then
+            table.remove(chainObj.tasks, i)
+            break
+        end
+    end
+end
+
+
+---.. versionadded:: v1.5.0
+---
+---Returns whether the chain has a task with that name. If not then the task is no longer running.
+---@param chain string
+---@param name string
+---@return boolean
+---@nodiscard
+function TaskManager.hasTask(chain, name)
+    local chainObj = chains[chain]
+    assert(
+        chainObj ~= nil,
+        "task chain " .. chain .. " does not exist"
+    )
+
+    local task = chainObj.taskMap[name]
+
+    return task ~= nil and not task.removed
 end
 
 
 local function updateTasks()
-    for i = #tasks, 1, -1 do
-        local success, result = pcall(tasks[i])
-        if not success then
-            ---@cast result string
-            print("Error occured in task: " .. result)
-            table.remove(tasks, i)
-        elseif result == TaskManager.TaskResult.DONE then
-            table.remove(tasks, i)
-        else
-            assert(
-                result == TaskManager.TaskResult.CONTINUE,
-                "Task did not return a valid result"
+    for _, chain in pairs(chains) do
+        executingChain = chain.name
+
+        for i = #chain.tasks, 1, -1 do
+            local task = chain.tasks[i]
+            local success, result = pcall(
+                task.func,
+                unpack(task.args)
             )
+
+            if not success then
+                ---@cast result string
+                log:warn(
+                    "error occured in chain %s task %s: %s",
+                    chain.name,
+                    task.name,
+                    result
+                )
+                task.removed = true
+            elseif result == TaskManager.TaskResult.DONE then
+                task.removed = true
+            elseif result == TaskManager.TaskResult.CONTINUE then
+                -- do nothing lol
+            else
+                pcall(
+                    log.error,
+                    log,
+                    "chain %s task %s did not return a valid result",
+                    chain.name,
+                    task.name
+                )
+                task.removed = true
+            end
+        end
+
+        -- remove tasks that were queued for removal during execution
+        for i = #chain.tasks, 1, -1 do
+            local task = chain.tasks[i]
+            if task.removed then
+                table.remove(chain.tasks, i)
+            end
         end
     end
+
+    executingChain = nil
 end
 
 
@@ -104,7 +247,7 @@ end
 
 ---@param ticks number
 function TaskManager.update(ticks)
-    currentTick = ticks
+    currentTick = math.floor(ticks)
 
     -- run repeat tasks
     for frequency, tasks in pairs(TaskManager.repeatTasks) do
--- docs/source/modules/TaskManager.rst
+++ docs/source/modules/TaskManager.rst
@@ -7,3 +7,87 @@ Many functions in this module take as argument a function to call and a list of 
 .. lua:automodule:: Starlit.TaskManager
    :members:
    :recursive:
+
+Enum
+----
+.. lua:autoenum:: starlit.taskmanager.TaskResult
+    :members:
+
+Examples
+--------
+A simple task that counts to 5 in the console:
+::
+
+    local TaskManager = require("Starlit/Taskmanager")
+
+    -- cache this so we don't have to type out the whole thing every time
+    local TaskResult = TaskManager.TaskResult
+
+    -- it is recommended to keep your task chain identifier in a variable to prevent typos
+    local TASK_CHAIN = "mymod.mymodule"
+    TaskManager.addTaskChain(TASK_CHAIN)
+
+
+    local count = 0
+    local function task()
+        count = count + 1
+        print(count)
+        if count == 5 then
+            -- reset the count for next time
+            count = 0
+            -- return DONE to tell the task manager to stop running this function every tick
+            return TaskResult.DONE
+        end
+
+        -- continue running the task on the next tick
+        return TaskResult.CONTINUE
+    end
+
+    -- start running the task next tick
+    TaskManager.addTask(TASK_CHAIN, task)
+
+An issue with this basic example is that `count` escapes the function, so we can't run more than one of the same task at the same time,
+they would share the same counter.
+To avoid this, we could create an object to store the state of the function, however this adds a lot of complexity to a very simple program.
+An alternative solution would be to make our function async, keeping all of the state within the function.
+An async function is essentially a function that can be paused, refered to as 'yielding'.
+To call an async function, we have to create a coroutine with that function.
+::
+
+    local TaskManager = require("Starlit/Taskmanager")
+
+    local TaskResult = TaskManager.TaskResult
+
+    local TASK_CHAIN = "mymod.mymodule"
+    TaskManager.addTaskChain(TASK_CHAIN)
+
+    -- When using EmmyLua, the @async annotation tells the language server that this an async function:
+    -- an async function is a function that is meant to be used in a coroutine.
+    -- this will generate a warning when you try to call it outside of another async function:
+    -- this is important because coroutine.yield will throw an error if you aren't in a coroutine!
+
+    ---@async
+    local function task()
+        for i = 1, 5 do
+            print(i)
+            -- coroutine.yield pauses the function
+            -- CONTINUE tells the task manager we want to resume from here next tick
+            coroutine.yield(TaskResult.CONTINUE)
+        end
+
+        return TaskResult.DONE
+    end
+
+    -- we can now add as many of the task as we want without issue:
+    TaskManager.addTask(
+        TASK_CHAIN,
+        -- we cannot pass an async function directly, we must create a new coroutine for that function with coroutine.wrap
+        coroutine.wrap(task)
+    )
+    TaskManager.addTask(
+        TASK_CHAIN,
+        coroutine.wrap(task)
+    )
+
+While the concepts of async functions and coroutines may be more difficult to grasp at first,
+you can clearly see how they allow us to write tasks with much more natural and simple code.
