a86a510f6e46f2dcd7979f669e109992865c99da
demiurgeQuantified <8355611+demiurgeQuantified@users.noreply.github.com>
2025-12-04 21:13:34 +0000

    simplify EntityHandle API

--- Contents/mods/StarlitLibrary/42/media/lua/shared/Starlit/EntityHandle.lua
+++ Contents/mods/StarlitLibrary/42/media/lua/shared/Starlit/EntityHandle.lua
@@ -1,16 +1,3 @@
-local TaskManager = require("Starlit/TaskManager")
-
-
-local TASK_CHAIN = "starlit.entityhandle"
-
-TaskManager.addTaskChain(TASK_CHAIN)
-
-
--- maximum number of entities to check for aliveness each tick
--- this isn't that expensive so it can be fairly high, but if it's too high it may not actually save performance
-local MAX_ENTITY_CHECKS_TICK = 256
-
-
 ---@namespace starlit
 
 
@@ -19,7 +6,7 @@ local MAX_ENTITY_CHECKS_TICK = 256
 ---
 ---The contained entity.
 ---It is recommended to use :lua:meth:`get()` to access the entity instead.
----@field entity T | nil
+---@field _entity T
 ---
 ---Cache of entity id to verify that the entity has not changed.
 ---@field _id number
@@ -27,69 +14,17 @@ local __GameEntityHandle = {}
 __GameEntityHandle.__index = __GameEntityHandle
 
 
----Returns the contained entity.
----Throws an exception if the handle is empty.
----@return T entity
+---Returns the contained entity, or nil if it has unloaded.
+---@return T? entity
 ---@nodiscard
 function __GameEntityHandle:get()
-    assert(not self:isEmpty(), "tried to access empty GameEntityHandle")
-    ---@cast self.entity -nil
-    return self.entity
-end
-
-
----Whether the contained entity still exists.
----An empty handle will never be non-empty again.
----@return boolean empty
----@nodiscard
-function __GameEntityHandle:isEmpty()
-    if not self.entity then
-        return true
-    end
-
-    
-    local id = self.entity:getEntityNetID()
+    local id = self._entity:getEntityNetID()
     if id == -1 or id ~= self._id then
-        return true
+        return nil
     end
 
-    return false
+    return self._entity
 end
-
-
----@generic T: GameEntity
----@type {[T] : EntityHandle<T>}
-local handleCache = {}
-
-
----@async
-local function releaseEmptyHandles()
-    local iterations = 0
-    while true do
-        for k, handle in pairs(handleCache) do
-            ---@cast k GameEntity
-            ---@cast handle EntityHandle
-            if handle:isEmpty() then
-                handle.entity = nil
-                handleCache[k] = nil
-            end
-
-            iterations = iterations + 1
-            if iterations > MAX_ENTITY_CHECKS_TICK then
-                coroutine.yield(TaskManager.TaskResult.CONTINUE)
-                iterations = 0
-            end
-        end
-
-        coroutine.yield(TaskManager.TaskResult.CONTINUE)
-        iterations = 0
-    end
-end
-
-TaskManager.addTask(
-    TASK_CHAIN,
-    coroutine.wrap(releaseEmptyHandles)
-)
 
 
 ---.. versionadded:: v1.5.0
@@ -104,20 +39,13 @@ local EntityHandle = {}
 ---@return EntityHandle<T> handle Handle for the entity.
 ---@nodiscard
 function EntityHandle.get(entity)
-    local handle = handleCache[entity]
-
-    if not handle or handle:isEmpty() then
-        handle = setmetatable(
-            {
-                entity = entity,
-                _id = entity:getEntityNetID()
-            },
-            __GameEntityHandle
-        ) ---@as EntityHandle<T>
-        handleCache[entity] = handle
-    end
-
-    return handle
+    return setmetatable(
+        {
+            _entity = entity,
+            _id = entity:getEntityNetID()
+        },
+        __GameEntityHandle
+    )
 end
 
 
