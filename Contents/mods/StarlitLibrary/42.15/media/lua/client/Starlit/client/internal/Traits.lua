d7bb43effab3619b440528b64dbf575a1a692748
albion <8355611+demiurgeQuantified@users.noreply.github.com>
2026-08-19 23:33:41 +0100

    trait sandbox options rewrite, simplifies code and fixes some apparent bugs (#69)

--- Contents/mods/StarlitLibrary/42.15/media/lua/client/Starlit/client/internal/Traits.lua
+++ Contents/mods/StarlitLibrary/42.15/media/lua/client/Starlit/client/internal/Traits.lua
@@ -1,9 +1,45 @@
 local Traits = require("Starlit/sandbox/Traits")
 local SandboxUtils = require("Starlit/sandbox/SandboxUtils")
+local StarlitLog = require("Starlit/debug/StarlitLog")
 
 
 ---@type CharacterTraitDefinition
 local traitMetatable = __classmetatables[CharacterTraitDefinition.class].__index
+
+
+---@param trait CharacterTrait
+---@return boolean
+---@nodiscard
+local function isTraitAvailable(trait)
+    local data = Traits.traitInfos[trait]
+    local definition = CharacterTraitDefinition.getCharacterTraitDefinition(trait)
+
+    if data.toggleOption ~= "" then
+        local available = SandboxUtils.getOptionValue(data.toggleOption)
+        if type(available) ~= "boolean" then
+            StarlitLog:warn(
+                "Value of trait '%s' toggle sandbox option '%s' is not a boolean",
+                trait:getName(),
+                data.toggleOption
+            )
+            available = true
+        elseif not available then
+            return false
+        end
+    end
+
+    -- don't make traits mutually exclusive with ones you already have selected available
+
+    local items = MainScreen.instance--[[@cast -?]].charCreationProfession.listboxTraitSelected.items
+    for i = 1, #items do
+        local item = items[i].item ---@as CharacterTraitDefinition
+        if item:isMutuallyExclusive(definition) then
+            return false
+        end
+    end
+
+    return true
+end
 
 
 --FIXME: it shows in the wrong list if you change the price and then go back to a preset difficulty (who cares, low prio)
@@ -15,42 +51,49 @@ local function updateTraits()
         local traitDef = CharacterTraitDefinition.getCharacterTraitDefinition(trait)
         local label = traitDef:getLabel()
 
-        if data.toggleOption ~= "" and not SandboxUtils.getOptionValue(data.toggleOption) then
-            ccp.listboxTrait:removeItem(label)
-            ccp.listboxBadTrait:removeItem(label)
-            ccp.listboxTraitSelected:removeItem(label)
-            data.lastCost = nil
-        elseif data.costOption ~= "" then
-            local newCost = traitDef:getCost()
+        local available = isTraitAvailable(trait)
+        local cost = traitDef:getCost()
 
-            local costChange = newCost - data.lastCost
+        local hadTrait = ccp.listboxTraitSelected:contains(label)
 
-            if costChange > ccp.pointToSpend then
-                ccp.listboxTrait:removeItem(label)
-                ccp.listboxBadTrait:removeItem(label)
-                if ccp.listboxTraitSelected:removeItem(label) then
-                    ccp.pointToSpend = ccp.pointToSpend + data.lastCost
-                end
+        ccp.listboxTrait:removeItem(label)
+        ccp.listboxBadTrait:removeItem(label)
+        ccp.listboxTraitSelected:removeItem(label)
+    
+        if hadTrait then
+            ccp.pointToSpend = ccp.pointToSpend + data.lastCost
+        end
 
-                local item
-                if newCost > 0 then
-                    item = ccp.listboxTrait:addItem(label, traitDef)
-                else
-                    item = ccp.listboxBadTrait:addItem(label, traitDef)
-                end
+        if not available then
+            data.lastCost = 0
+        else
+            local item
 
-                item.tooltip = traitDef:getDescription()
+            if hadTrait then
+                item = ccp.listboxTraitSelected:addItem(label, traitDef)
+                ccp.pointToSpend = ccp.pointToSpend - cost
+            elseif cost > 0 then
+                item = ccp.listboxTrait:addItem(label, traitDef)
             else
-                ccp.pointToSpend = ccp.pointToSpend + -costChange
+                item = ccp.listboxBadTrait:addItem(label, traitDef)
             end
 
-            data.lastCost = newCost
+            item.tooltip = traitDef:getDescription()
+
+            data.lastCost = cost
         end
     end
 
     CharacterCreationMain.sort(ccp.listboxTrait.items)
     CharacterCreationMain.invertSort(ccp.listboxBadTrait.items)
     CharacterCreationMain.sort(ccp.listboxTraitSelected.items)
+end
+
+local old_repopulate = CharacterCreationProfession.repopulateTraitLists
+
+function CharacterCreationProfession:repopulateTraitLists()
+    old_repopulate(self)
+    updateTraits()
 end
 
 
